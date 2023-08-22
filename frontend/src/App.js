0fc1b50b55c4e2fa97ecc6c667c17eeff883370f
steward <bryanherreraestrada@gmail>
2023-08-21 21:46:54 -0600

    Added frontend to get all the ToDos from the database, create a ToDo in the frontend and update the ToDo and added some styles to it

---
+++ frontend/src/App.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import { addToDo, getAllToDo, updateToDo } from "./utils/HandleApp";
+
+
+function App() {
+
+  // Add a ToDO
+  const [toDo, setToDo] = useState([])
+  const [text, setText] = useState("")
+
+  // update a ToDO
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [toDoId, setToDoId] = useState("")
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  // activate Update mode
+  const updateMode = (_id, text) => {
+    setIsUpdating(true)
+    setText(text)
+    setToDoId(_id)
+  }
+
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>ToDo App</h1>
+
+        <div className="top">
+          <input 
+            type="text" 
+            placeholder="Add ToDo" 
+            value={text}
+            onChange={(e) => setText(e.target.value)}>
+
+              
+          </input>
+          <div
+            className="add"
+            onClick={isUpdating ?
+              () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+              : () => addToDo(text, setText, setToDo)}>
+            {isUpdating ? "Update" : "Add"}
+          </div>
+          </div>
+
+        <div className="list">
+
+          {toDo.map((item) => 
+          <ToDo 
+            key={item._id} 
+            text={item.text} 
+            updateMode={() => updateMode(item._id, item.text)}
+          /> 
+          )}
+
+          
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/utils/HandleApp.js
@@ -0,0 +1,43 @@
+// Connect with the backend
+
+import axios from 'axios'
+
+// The Url of the database
+const baseUrl = "http://localhost:5000"
+
+// get all the ToDos from the database
+const getAllToDo = (setToDo) => {
+    axios
+    .get(baseUrl)
+    .then(({data}) => {
+        console.log('data --->', data);
+        setToDo(data)
+    })
+}
+
+// This function adds a ToDo in the database 
+const addToDo = (text, setText, setToDo) => {
+    axios
+    .post(`${baseUrl}/save`, {text})
+    .then((data) => {
+        console.log(data);
+        setText("")
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+// This function updates a ToDo in the database 
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+    axios
+    .post(`${baseUrl}/update`, {_id: toDoId, text})
+    .then((data) => {
+        setText("")
+        setIsUpdating(false)
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+// export it to use in other files
+export {getAllToDo, addToDo, updateToDo}
